11dca26f9ae2ca76b4862beb0cf730550dcbcb8b
Aniket <aniketraj.go@gmail.com>
2021-01-13 13:06:26 +0530

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,39 @@ class HomePage extends StatelessWidget {
         title: Text("Awesome App Home"),
         backgroundColor: Colors.deepPurple[900],
       ),
-      body: Container(
-        child: Center(child: Text('Hello Raj')),
+      body: Center(
+        child: Container(
+          //color: Colors.teal,
+          width: 200,
+          height: 200,
+          alignment:
+              Alignment.center, // to align the child within the container
+          decoration: BoxDecoration(
+            // to decorate the container but if u are using this block then donot use color property of container outside the boxDecoration otherwise error
+            //color: Colors.teal,
+            shape: BoxShape.circle,
+            //borderRadius: BorderRadius.circular(20), // this will not work with shape
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blueGrey,
+                blurRadius: 10,
+                offset: Offset(2.0, 8.0),
+              )
+            ],
+            gradient:
+                LinearGradient(colors: [Colors.blueAccent, Colors.pinkAccent]),
+          ),
+          child: Text(
+            "I am a box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5,
+            ),
+          ),
+        ),
       ),
     );
   }
