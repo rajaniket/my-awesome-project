c3ee828e94357db03472a1d1058cae91f5f7ad2c
Aniket <aniketraj.go@gmail.com>
2021-02-12 12:45:00 +0530

    updated

--- lib/utiles/constants.dart
+++ lib/utiles/constants.dart
@@ -2,6 +2,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Constants {
   // this is native property and it includs kotlin so restart of app is required
-  static SharedPreferences
-      pref; // A static function is a member function of a class that can be called even when an object of the class is not initialized
+  static SharedPreferences pref;
+  // The static keyword is used to declare the class variable and method. It generally manages the memory for the global data variable.
+  // A static function is a member function of a class that can be called even when an object of the class is not initialized.
+  // if we declare the data member as static then we can access it without creating an object.
 }
